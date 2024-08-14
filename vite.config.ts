4750549b06d8566659126b213876a5412fdf5b34
R.Moroizumi <m.ryousuke0401@icloud.com>
2024-08-14 17:18:55 +0900

    :wrench: Config: flat-routes init

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { vitePlugin as remix } from '@remix-run/dev';
+import { flatRoutes } from 'remix-flat-routes';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -9,6 +10,18 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+      },
+      routes: async (defineRoutes) => {
+        return flatRoutes('routes', defineRoutes, {
+          ignoredRouteFiles: [
+            '.*',
+            '**/*.css',
+            '**/*.test.{js,jsx,ts,tsx}',
+            '**/__*.*',
+            '**/*.server.*',
+            '**/*.client.*',
+          ],
+        });
       },
     }),
     tsconfigPaths(),
